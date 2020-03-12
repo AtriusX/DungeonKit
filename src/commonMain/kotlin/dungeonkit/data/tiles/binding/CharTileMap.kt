bf6357939f5f63b4d9d912dac5fc63ffaaa57628
Atrius <atriusedm@gmail.com>
2020-03-12 18:56:42 -0400

    Updating the primary tilemaps

--- src/commonMain/kotlin/dungeonkit/data/tiles/binding/CharTileMap.kt
+++ src/commonMain/kotlin/dungeonkit/data/tiles/binding/CharTileMap.kt
@@ -1,20 +1,20 @@
 package dungeonkit.data.tiles.binding
 
 import dungeonkit.data.tiles.SimpleTile
+import dungeonkit.data.tiles.Tile
 import dungeonkit.data.tiles.Tiles
 
 /**
  * A simple tile map binding tiles to [Characters][Char]. The primary use-case for
  * this tile map is for text-based renderers like console windows.
  */
-open class CharTileMap : TileMap<Char> {
+object SimpleCharTileMap : TileMap<Char> {
 
-    override val default: TileBinding<SimpleTile, Char> =
-        Tiles.WALL bind '#'
+    override val tiles: Set<TileBinding<Tile, Char>> = setOf(
+        Tiles.WALL  bind '#',
+        Tiles.FLOOR bind ' ',
+        Tiles.EXIT  bind '.'
+    )
 
-    override val primary: TileBinding<SimpleTile, Char> =
-        Tiles.FLOOR bind ' '
-
-    override val secondary: TileBinding<SimpleTile, Char> =
-        Tiles.EXIT bind '.'
+    override val default: TileBinding<Tile, Char> = this["wall"]
 }
