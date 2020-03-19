1f5667285a4cb0edee410611ed1af70e83ba2485
Atrius <atriusedm@gmail.com>
2020-03-19 00:10:39 -0400

    Add API documentation

--- src/mingwMain/kotlin/dungeonkit/Log.kt
+++ src/mingwMain/kotlin/dungeonkit/Log.kt
@@ -1,5 +1,11 @@
 package dungeonkit
 
+/**
+ * @suppress Native implementation is unable to utilize the [IO][kotlin.io]
+ *           package currently. So the native branch can't support logging
+ *           through this API. println() should be utilized where possible
+ *           instead.
+ */
 actual object Log {
     actual fun info(message: Any?)  = Unit
     actual fun warn(message: Any?)  = Unit
