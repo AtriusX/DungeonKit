5acaa8e4f2dcf37824ad9be1ff0ed63800eec4da
Atrius <atriusedm@gmail.com>
2020-03-22 17:13:50 -0400

    Simplify the Title object and update generator

--- src/commonMain/kotlin/dungeonkit/text/Title.kt
+++ src/commonMain/kotlin/dungeonkit/text/Title.kt
@@ -1,22 +1,24 @@
 package dungeonkit.text
 
 object Title {
+    // Provided default formatters
     private val defaults = arrayOf(
         "%4na, The %a %no of %3na",
         "%a %no"
     )
 
     fun generate(format: String = defaults.random()) =
-        format.split(" ").map {
+        format.split(" ").joinToString(" ") {
             Token.parse(it)?.run {
-                when(token) {
+                when (token) {
                     "na" -> Selector.NAME(count)
                     "no" -> Selector.NOUN()
                     "a"  -> Selector.ADJECTIVE()
+                    "r"  -> Selector.RANDOM(count)
                     else -> null
                 }?.build()
             } ?: it
-        }.joinToString(" ")
+        }
 
     private data class Token(
         val count: Int,
@@ -30,34 +32,5 @@ object Title {
                 return Token(groups[1].toIntOrNull() ?: 1, groups[2])
             }
         }
-    }
-
-    private sealed class Selector(
-        vararg val data: String
-    ) {
-        open fun build() = data.random()
-
-        class NAME(
-            private val    count: Int,
-                    vararg data : String
-        ) : Selector(
-            "se", "tu", "xa", "eiq", "aw", "ret", "wer", "xun", "op",
-            "slb", "ijv", "oj", "jo", "owd", "ih", "ou", "qin", "mo", "ku",
-            "ur", "io", "xe", *data
-        ) {
-            override fun build() = Array(count) { data.random() }.joinToString("")
-        }
-
-        class NOUN(
-            vararg data: String
-        ) : Selector(
-            "cavern", "fields", "keep", "passageway", "hall", "lake", "hive", "dimension", *data
-        )
-
-        class ADJECTIVE(
-            vararg data: String
-        ) : Selector(
-            "hidden", "serene", "wandering", "pleasant", "white", "violent", *data
-        )
     }
 }
