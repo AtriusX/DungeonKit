3b82725892c0698bbfeeb5ea9a1ef9d9c4b017df
Atrius <atriusedm@gmail.com>
2020-03-26 23:46:50 -0400

    Add collision detection to Room API

--- src/commonMain/kotlin/dungeonkit/data/Room.kt
+++ src/commonMain/kotlin/dungeonkit/data/Room.kt
@@ -9,23 +9,23 @@ import kotlin.collections.MutableMap.MutableEntry as Entry
  * grid data. This class also takes care to explicitly initialize all values within
  * it's bounds.
  *
- * @param coordinate The [Coordinate] of this room corresponding to it's position on
+ * @param pos The [Coordinate] of this room corresponding to it's position on
  *                   existing [Grid] data.
  * @param dimension  The width and height of this room.
  * @param floor      The floor [Tile] used for this room.
  */
 class Room(
-    val coordinate: Coordinate,
-    val dimension : Dimension,
-        floor     : Tile
+    val pos      : Coordinate,
+    val dimension: Dimension,
+    floor        : Tile
 ): Grid<Tile>(dimension, floor) {
     init {
         val (w, h) = dimension
         // Ensure the room has space before initializing
         if (w != 0 && h != 0) {
             // Fill in the entire floor explicitly with the floor tile.
-            val (begX, begY) = coordinate
-            val (endX, endY) = coordinate + dimension
+            val (begX, begY) = pos
+            val (endX, endY) = pos + dimension
             for (x in begX until endX)
                 for (y in begY until endY)
                     this[x, y] = floor
@@ -63,22 +63,22 @@ class Room(
      */
     val tiles: Grid<Tile>
         get() = also {
-            forEach { (c, _) -> c.x += coordinate.x; c.y += coordinate.y }
+            forEach { (c, _) -> c.x += pos.x; c.y += pos.y }
         }
 
     /**
      * @property center The center-point of this room represented as a [Coordinate]
      */
     val center: Coordinate
-        get() = area.w / 2 + coordinate.x at
-                area.h / 2 + coordinate.y
+        get() = area.w / 2 + pos.x at
+                area.h / 2 + pos.y
 
     /**
      * @property border Retrieves a list of coordinates that edge the room.
      */
     val border: List<Coordinate>
         get() = map(Entry<Coordinate, Tile>::key).filter {
-            it.x - coordinate.x !in 1 until area.w || it.y - coordinate.y !in 1 until area.h
+            it.x - pos.x !in 1 until area.w || it.y - pos.y !in 1 until area.h
         }
 
     /**
@@ -90,7 +90,7 @@ class Room(
      * @return  The [Tile] stored at the specified x/y point.
      */
     override operator fun get(x: Int, y: Int) =
-        super.get(x - coordinate.x, y - coordinate.y)
+        super.get(x - pos.x, y - pos.y)
 
     /**
      * Sets a tile at the specified point in the room. Parameters are rebased
@@ -101,5 +101,26 @@ class Room(
      * @param value The value to set at the specified point.
      */
     override operator fun set(x: Int, y: Int, value: Tile) =
-        super.set(x - coordinate.x, y - coordinate.y, value)
+        super.set(x - pos.x, y - pos.y, value)
+
+    /**
+     * Checks if another room overlaps the bounds of this one. This can be used
+     * to prevent rooms from overlaying on top of each other.
+     *
+     * @param other The room instance to check for overlaps.
+     * @return      True if the rooms overlap and false otherwise.
+     */
+    operator fun contains(other: Room): Boolean {
+        val (x, y)       = pos
+        val (endX, endY) = pos + dimension
+        for (dx in x until endX) for (dy in y until endY)
+            if (other hasPosition (dx at dy)) return true
+        return false
+    }
+
+    private infix fun hasPosition(coordinate: Coordinate): Boolean {
+        val (x, y)       = pos
+        val (endX, endY) = pos + dimension
+        return coordinate.x in x..endX && coordinate.y in y..endY
+    }
 }
