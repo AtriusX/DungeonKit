777fbd1dd0742617c9e4ed8522a561196b1c7c2b
Atrius <atriusedm@gmail.com>
2020-04-04 04:09:51 -0400

    Update Modifer
    It should fall under the dungeon process hierarchy

--- src/commonMain/kotlin/dungeonkit/data/steps/modifiers/Modifier.kt
+++ src/commonMain/kotlin/dungeonkit/data/steps/modifiers/Modifier.kt
@@ -1,17 +1,19 @@
 package dungeonkit.data.steps.modifiers
 
+import dungeonkit.DungeonProcess
+
 /**
  * Designates a generation modifier. Any class that extends this interface may be used as
  * a modifier in the generator. Modifiers **should not** however implement this interface
  * directly. Classes should instead implement extensions of this interface. The purpose of
  * this is to enable data-specific use cases within different steps.
  */
-interface Modifier {
+interface Modifier : DungeonProcess {
 
     /**
      * @property status Describes what the modifier is doing. Can be used to relay information
      *                  back to the user.
      */
-    val status: String
+    override val status: String
         get() = "Modifying generation..."
 }
