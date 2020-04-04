6530fb0b248419697264b2c269b2ac085cc86ba2
Atrius <atriusedm@gmail.com>
2020-04-04 04:10:22 -0400

    Add exit generator and modifier

---
+++ src/commonMain/kotlin/dungeonkit/data/steps/modifiers/Exit.kt
@@ -0,0 +1,29 @@
+package dungeonkit.data.steps.modifiers
+
+import dungeonkit.data.Grid
+import dungeonkit.data.Room
+import dungeonkit.data.steps.Step
+import dungeonkit.data.tiles.Tile
+import dungeonkit.data.tiles.binding.TileMap
+
+/**
+ * A simple generator that applies an exit to the given dungeon floor. This keeps
+ * things modularized away from other scripts so this doesn't need to generate over
+ * top of other generators directly.
+ *
+ * @property exit The name of the exit tile to apply to the grid.
+ * @constructor   Constructs a new exit generator.
+ */
+class Exit(
+    private val exit: String = "exit"
+) : Step, RoomModifier {
+    override val status: String
+        get() = "Dropping exit..."
+
+    override fun process(map: Grid<Tile>, tileMap: TileMap<*>) = map.also {
+        map.shuffled().random().setValue(tileMap[exit].tile)
+    }
+
+    override fun modify(map: Grid<Tile>, tileMap: TileMap<*>, rooms: List<Room>) =
+        process(rooms.random().tiles, tileMap)
+}
