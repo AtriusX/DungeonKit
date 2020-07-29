eff2d2ef7760130d7b1708baad6650bba95f043a
Atrius <atriusedm@gmail.com>
2020-07-29 03:09:54 -0400

    First implementation of RectCell

--- src/commonMain/kotlin/dungeonkit/data/steps/RectCell.kt
+++ src/commonMain/kotlin/dungeonkit/data/steps/RectCell.kt
@@ -3,6 +3,7 @@ package dungeonkit.data.steps
 import dungeonkit.data.Dimension
 import dungeonkit.data.Grid
 import dungeonkit.data.Room
+import dungeonkit.data.at
 import dungeonkit.data.steps.modifiers.Modifier
 import dungeonkit.data.tiles.Tile
 import dungeonkit.data.tiles.binding.TileMap
@@ -10,25 +11,35 @@ import dungeonkit.dim
 import kotlin.random.Random
 
 open class RectCell(
-    private         val cellSize : Dimension = 5.dim,
-    private         val roomCount: IntRange  = 6..10,
-    private         val floorTile: String    = "floor",
-    override vararg val modifiers: Modifier
+    private         val cellSize       : Dimension = 5.dim,
+    private         val roomCount      : IntRange  = 6..10,
+    private         val hallwayDistance: Int       = 1,
+    private         val floorTile      : String    = "floor",
+    override vararg val modifiers      : Modifier
 ) : ModifiableStep {
 
     override fun process(map: Grid<Tile>, tileMap: TileMap<*>): Grid<Tile> {
         if (map.area.h < cellSize.h || map.area.w < cellSize.w)
             throw IllegalStateException("Cannot generate cell grid in map: Too small")
-
-        val viableArea = map.area - cellSize
-        val rooms = mutableListOf(Room(viableArea.random(), cellSize, tileMap[floorTile].tile))
+        val viableArea    = map.area - cellSize
+        val tile          = tileMap[floorTile].tile
+        val rooms         = mutableListOf(Room(viableArea.random(), cellSize, tile))
         val roomsToCreate = roomCount.random()
+        // Create the decided room amount
         while (rooms.size < roomsToCreate) {
-            val room = rooms.random()
-            val sides = listOf(room.top, room.left, room.bottom, room.right)
+            val room  = rooms.random()
             val index = Random.nextInt(4)
-            // TODO: If index is even then it's a top of bottom border, if not then it's
-            //       a left or right border. 
+            // Calculate the offset coordinate vector for the generated room
+            val offset = if (index % 2 == 0) (index - 2) at 0 else 0 at (index - 1)
+            val pos    = room.pos + (offset * cellSize + hallwayDistance)
+            if (pos !in viableArea)
+                continue
+            val newRoom = Room(pos, cellSize, tile)
+            // Add rooms to running total and apply to map
+            rooms += newRoom
+            map   += newRoom
+            // Generate pathway (Not probably the most efficient way, but it should work)
+            Path(room.center, newRoom.center).process(map, tileMap)
         }
         return map
     }
